c51be9fc14d3a0b1105f87cd6e5cedad02a96faf
Nguyen Linh <nguyenphuonglinh.0795@gmail.com>
2019-10-04 20:47:52 +0700

    day-2-imic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome Linh to Open sourse reactjs
+    </div>
+  );
+}
+
+export default App;
